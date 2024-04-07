bff30952e070faf81653ef689f9849e65468c485
Alina Zhulanava <zhulanavaa@gmail.com>
2024-04-07 22:33:30 +0200

    final version

--- files_processing/FileMerger.py
+++ files_processing/FileMerger.py
@@ -1,5 +1,3 @@
-from typing import TextIO
-
 from files_processing.PathProvider import get_path
 from files_processing.UsersProcessor import read_users, write_users
 from files_processing.BooksProcessor import read_books
@@ -13,7 +11,6 @@ class FileMerger:
     def __init__(self):
         pass
 
-
     def give_books_to_users(self):
         with open(PATH_USERS, "r") as f:
             self.users_list = read_users(f)
@@ -24,14 +21,11 @@ class FileMerger:
         self.books_list.reverse()
 
         give_iterations = len(self.books_list) / len(self.users_list)
-        while (give_iterations>0):
+        while (give_iterations > 0):
             for user in self.users_list:
                 if self.books_list:
                     user["books"].append(self.books_list.pop())
-            give_iterations = give_iterations -1
-
-
-
+            give_iterations = give_iterations - 1
 
     def return_merged_files(self):
         with open(PATH_RESULT, "a") as f:
--- files_processing/UsersProcessor.py
+++ files_processing/UsersProcessor.py
@@ -5,10 +5,14 @@ def read_users(file_users):
     users = json.loads(file_users.read())  # returns a list of objects
     filtered_users = []
     for user in users:
-        user_dict = {"name": user["name"], "gender": user["gender"], "address": user["address"], "age": user["age"],
+        user_dict = {"name": user["name"],
+                     "gender": user["gender"],
+                     "address": user["address"],
+                     "age": user["age"],
                      "books": []}
         filtered_users.append(user_dict)
     return filtered_users
+
 
 def write_users(file_result, users_with_books):
     piece_of_json = json.dumps(users_with_books)
